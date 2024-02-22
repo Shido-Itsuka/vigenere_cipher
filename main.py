487e0fb5c43cdf7ae19e761b8029368a237dac9a
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-02-22 14:49:26 +1000

    - working navigation - beautiful design - test code page, so it will be reworked - now user can minimize the window - window width = 900

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flet as ft
 from flet_core.control import Control
+import module
 
 
 def main(page: ft.Page) -> None:
@@ -8,60 +9,187 @@ def main(page: ft.Page) -> None:
     page.theme_mode.SYSTEM = True
 
     page.window_maximizable = False
-    page.window_minimizable = False
+    page.window_minimizable = True
     page.window_resizable = False
 
     page.window_height = 600
-    page.window_width = 800
+    page.window_width = 900
+    page.padding = 0
     page.window_center()
 
     page.theme = ft.Theme(
         color_scheme_seed='#5a84ff'
     )
 
+    def on_change_rail(e):
+        match e.control.selected_index:
+            case 0:
+                main_body.content = code_body
+            case 1:
+                main_body.content = decode_body
+            case 2:
+                main_body.content = settings_body
+            case 3:
+                main_body.content = info_body
+        page.update()
+
     rail = ft.NavigationRail(
         selected_index=0,
-        label_type=ft.NavigationRailLabelType.ALL,
+        label_type=ft.NavigationRailLabelType.SELECTED,
+        bgcolor=ft.colors.TRANSPARENT,
         min_width=100,
         min_extended_width=200,
         group_alignment=0,
-
+        on_change=on_change_rail,
+        indicator_color=ft.colors.TRANSPARENT,
         destinations=[
             ft.NavigationRailDestination(
-                icon=ft.icons.ENHANCED_ENCRYPTION_OUTLINED,
-                selected_icon=ft.icons.ENHANCED_ENCRYPTION,
+                icon_content=ft.Icon(
+                    name=ft.icons.ENHANCED_ENCRYPTION_OUTLINED,
+                    color=ft.colors.WHITE54
+                ),
+                selected_icon_content=ft.Icon(
+                    name=ft.icons.ENHANCED_ENCRYPTION,
+                    color=ft.colors.WHITE
+                ),
+                label='Шифрование',
+                padding=20
             ),
             ft.NavigationRailDestination(
-                icon=ft.icons.NO_ENCRYPTION_OUTLINED,
-                selected_icon=ft.icons.NO_ENCRYPTION,
+                icon_content=ft.Icon(
+                    name=ft.icons.NO_ENCRYPTION_OUTLINED,
+                    color=ft.colors.WHITE54
+                ),
+                selected_icon_content=ft.Icon(
+                    name=ft.icons.NO_ENCRYPTION,
+                    color=ft.colors.WHITE
+                ),
+                label='Дешифрование',
+                padding=20
             ),
             ft.NavigationRailDestination(
-                icon=ft.icons.SETTINGS_OUTLINED,
-                selected_icon=ft.icons.SETTINGS,
+                icon_content=ft.Icon(
+                    name=ft.icons.SETTINGS_OUTLINED,
+                    color=ft.colors.WHITE54
+                ),
+                selected_icon_content=ft.Icon(
+                    name=ft.icons.SETTINGS,
+                    color=ft.colors.WHITE
+                ),
+                label='Настройки',
+                padding=20
             ),
             ft.NavigationRailDestination(
-                icon=ft.icons.HELP_OUTLINE,
-                selected_icon=ft.icons.HELP
+                icon_content=ft.Icon(
+                    name=ft.icons.HELP_OUTLINE,
+                    color=ft.colors.WHITE54
+                ),
+                selected_icon_content=ft.Icon(
+                    name=ft.icons.HELP,
+                    color=ft.colors.WHITE
+                ),
+                label='О программе',
+                padding=20
             )
         ],
-        on_change=lambda e: print("Selected destination:", e.control.selected_index)
+    )
+
+    code_body = ft.Container(
+        expand=True,
+        alignment=ft.alignment.center,
+        content=ft.Row(
+            vertical_alignment=ft.alignment.center,
+            alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+            controls=[
+                ft.Column(
+                    controls=[
+                        ft.OutlinedButton(
+                            "Вставить",
+                            icon=ft.icons.PASTE
+                        ),
+
+                        ft.TextField(
+                            max_lines=10
+                        )
+                    ]
+                ),
+
+                ft.Column(
+                    controls=[
+                        ft.OutlinedButton(
+                            "Шифровать"
+                        ),
+                        ft.TextField(
+
+                        )
+                    ]
+                ),
+
+                ft.Column(
+                    controls=[
+                        ft.OutlinedButton(
+                            "Копировать",
+                            icon=ft.icons.COPY
+                        ),
+
+                        ft.TextField(
+                            read_only=True
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    decode_body = ft.Container(
+        expand=True,
+        alignment=ft.alignment.center,
+        content=ft.Text('Дешифрование')
+    )
+
+    settings_body = ft.Container(
+        expand=True,
+        alignment=ft.alignment.center,
+        content=ft.Text('Настройки')
+    )
+
+    info_body = ft.Container(
+        expand=True,
+        alignment=ft.alignment.center,
+        content=ft.Text('О программе')
+    )
+
+    main_body = ft.Container(
+        padding=0,
+        expand=True,
+        content=code_body
+    )
+
+    main_container = ft.Stack(
+        expand=True,
+        controls=[
+            ft.Image(src='assets/bg_low.png',
+                     width=page.window_width,
+                     height=page.window_height,
+                     fit=ft.ImageFit.COVER,
+                     ),
+            ft.Container(
+                padding=0,
+                content=ft.Row(
+                    spacing=0,
+                    controls=[
+                        rail,
+                        ft.VerticalDivider(width=1),
+                        main_body
+                    ],
+                    expand=True
+                ),
+                expand=True
+            )
+        ]
     )
 
     page.add(
-        ft.Row(
-            [
-                rail,
-                ft.VerticalDivider(width=1),
-                ft.Column(
-                    [
-                        ft.Text('DUDUDU!'),
-                    ],
-                    alignment=ft.MainAxisAlignment.START,
-                    expand=True
-                )
-            ],
-            expand=True
-        )
+        main_container
     )
 
     page.update()
