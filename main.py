43feb967267a5cf19e19105f69f5f5f7de2c24a9
Shido_Itsuka <trusoff.maksim2017@gmail.com>
2024-02-25 21:54:22 +1000

    -pretty code page -added several features that will work in future commits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
 from flet_core.control import Control
 import module
+import re
 
 
 def main(page: ft.Page) -> None:
@@ -21,6 +22,12 @@ def main(page: ft.Page) -> None:
         color_scheme_seed='#5a84ff'
     )
 
+    def custom_replacement_string(text):
+        pass
+
+    def custom_input_filter(lang: str):
+        pass
+
     def on_change_rail(e):
         match e.control.selected_index:
             case 0:
@@ -32,6 +39,45 @@ def main(page: ft.Page) -> None:
             case 3:
                 main_body.content = info_body
         page.update()
+
+    def on_change_text_input(e):
+        if e.control.value == '':
+            key_input.disabled = True
+            key_input.value = ''
+            e.control.error_text = None
+        else:
+            if re.fullmatch(r'[а-яА-ЯёЁ]+', e.control.value):
+                key_input.suffix_text = 'RU'
+                key_input.disabled = False
+                e.control.error_text = None
+            elif re.fullmatch(r'[a-zA-Z]+', e.control.value):
+                key_input.suffix_text = 'EN'
+                key_input.disabled = False
+                e.control.error_text = None
+            else:
+                e.control.error_text = 'Только русский или английский текст'
+                key_input.disabled = True
+
+        page.update()
+
+    key_input_memory: [str, str] = ['', '']
+
+    def key_input_change(e):
+        if e.control.value == '':
+            key_input_memory[0] = ''
+            key_input_memory[1] = ''
+
+        if key_input_memory[0] == '':
+            key_input_memory[0] = e.control.value
+        if key_input_memory[0] and key_input_memory[1] != '':
+            key_input_memory[0] = key_input_memory[1]
+            key_input_memory[1] = e.control.value
+        elif key_input_memory[0] != '':
+            key_input_memory[1] = e.control.value
+        else:
+            print(f'Непредвиденная ошибка: {key_input_memory}')
+
+        print(f'key_input_memory: {key_input_memory}\nkey_input.value: {key_input.value}')
 
     rail = ft.NavigationRail(
         selected_index=0,
@@ -97,43 +143,82 @@ def main(page: ft.Page) -> None:
     code_body = ft.Container(
         expand=True,
         alignment=ft.alignment.center,
-        content=ft.Row(
-            vertical_alignment=ft.alignment.center,
+        content=ft.Column(
+            horizontal_alignment=ft.alignment.center,
             alignment=ft.MainAxisAlignment.SPACE_EVENLY,
             controls=[
-                ft.Column(
+                ft.Row(
+                    spacing=20,
                     controls=[
-                        ft.OutlinedButton(
-                            "Вставить",
-                            icon=ft.icons.PASTE
+                        ft.IconButton(
+                            icon=ft.icons.PASTE,
+                            tooltip='Вставить',
+                            icon_color=ft.colors.ON_BACKGROUND,
                         ),
 
-                        ft.TextField(
-                            max_lines=10
+                        text_input := ft.TextField(
+                            max_lines=5,
+                            multiline=True,
+                            width=355,
+                            border_color=ft.colors.OUTLINE,
+                            focused_border_color=ft.colors.ON_SURFACE_VARIANT,
+                            label='Текст для шифрования',
+                            on_change=on_change_text_input
                         )
                     ]
                 ),
 
-                ft.Column(
+                ft.Row(
+                    spacing=20,
                     controls=[
-                        ft.OutlinedButton(
-                            "Шифровать"
+                        ft.IconButton(
+                            icon=ft.icons.CASINO_OUTLINED,
+                            tooltip='Генерировать'
                         ),
-                        ft.TextField(
-
+                        key_input := ft.TextField(
+                            width=355,
+                            border_color=ft.colors.OUTLINE,
+                            focused_border_color=ft.colors.ON_SURFACE_VARIANT,
+                            max_lines=2,
+                            max_length=33,
+                            label='Ключ',
+                            capitalization=ft.TextCapitalization.CHARACTERS,
+                            # input_filter=ft.TextOnlyInputFilter(),
+                            suffix_text='',
+                            on_change=key_input_change,
+                            disabled=True
+                        ),
+                        ft.VerticalDivider(
+                            visible=True,
+                            width=40
+                        ),
+                        ft.OutlinedButton(
+                            text='Шифровать',
+                            style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=5),
+                            ),
+                            scale=1.2
                         )
                     ]
                 ),
 
-                ft.Column(
+                ft.Row(
+                    spacing=20,
                     controls=[
-                        ft.OutlinedButton(
-                            "Копировать",
-                            icon=ft.icons.COPY
+                        ft.IconButton(
+                            icon=ft.icons.COPY,
+                            tooltip='Копировать'
                         ),
 
                         ft.TextField(
-                            read_only=True
+                            read_only=True,
+                            max_lines=5,
+                            multiline=True,
+                            width=355,
+                            border_color=ft.colors.OUTLINE,
+                            focused_border_color=ft.colors.ON_SURFACE_VARIANT,
+                            label='Зашифрованный текст'
+
                         )
                     ]
                 )
@@ -159,7 +244,8 @@ def main(page: ft.Page) -> None:
     )
 
     main_body = ft.Container(
-        padding=0,
+        padding=10,
+        margin=ft.margin.only(left=20),
         expand=True,
         content=code_body
     )
