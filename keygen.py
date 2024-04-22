d8f66440deb7df1f097225d0bdec3066ec7e3ebf
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-04-22 15:00:44 +1000

    -

--- keygen.py
+++ keygen.py
@@ -30,35 +30,41 @@ class KeyInput:
         try:
             len(self.key.split('-')) == 4
         except ValueError:
-            raise ValueError("Key must be in format XXXX-YYYY-ZZZZ-1234")
+            raise ValueError("Key must be in format 0XXXX-0YYYY-ZZZZ-1234")
 
     def part1_validate(self, part):
-        pass
+        return True
 
     def part2_validate(self, part):
-        pass
+        return True
 
     def part3_validate(self, part):
-        pass
+        return True
 
     def part4_validate(self, part):
-        pass
+        return True
 
     def validate(self):
-        if [len(x) == 4 for x in self.key.split('-')] and self.key.count('-') == 3:
+        if [len(x) == 5 for x in self.key.split('-')] and self.key.count('-') == 3:
             part1, part2, part3, part4 = self.key.split('-')
-            if all([self.part1_validate(part1), part2_validate(part2),
-                    part3_validate(part3), part4_validate(part4)]):
+            if all([self.part1_validate(part1), self.part2_validate(part2),
+                    self.part3_validate(part3), self.part4_validate(part4)]):
                 return True
             else:
-                raise ValueError("Key is incorrect")
+                # raise ValueError("Key is incorrect")
+                return False
         else:
-            raise ValueError("Key must be in format XXXX-YYYY-ZZZZ-1234")
+            # raise ValueError("Key must be in format XXXX-YYYY-ZZZZ-1234")
+            return False
 
     def main(self):
         if self.validate():
             print('Successful!')
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
-    main()
+    k = KeyInput('XXXX-YYYY-ZZZZ-1234')
+    k.main()
