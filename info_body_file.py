3a9296aebbb21bbba7de3d978a3797c21124d753
Shido's laptop <trusoff.maksim2017@gmail.com>
2024-09-18 11:53:02 +1000

    - add info about project, repository and author

--- info_body_file.py
+++ info_body_file.py
@@ -5,15 +5,67 @@ from flet_core.control import Control
 
 def info_page(page: ft.Page) -> Container:
     settings_body = Container(
-        expand=True,
+        # expand=True,
         alignment=ft.alignment.center,
         content=ft.Column(
-            horizontal_alignment=ft.alignment.center,
-            alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+            horizontal_alignment=ft.CrossAxisAlignment.START,
+            alignment=ft.MainAxisAlignment.START,
+            spacing=20,
             controls=[
-                ft.Row(
+                ft.Column(
                     controls=[
-                        ft.Text('О программе', weight=ft.FontWeight.BOLD),
+                        ft.Text(
+                            'Лозунговый шифр | Vigenere cipher',
+                            style=ft.TextThemeStyle.TITLE_SMALL,
+                            size=22
+                        ),
+                        ft.Text(
+                            'Версия | Version 0.9.0',
+                            style=ft.TextThemeStyle.BODY_MEDIUM,
+                            size=18
+                        ),
+                        ft.Text(
+                            spans=[
+                                ft.TextSpan(
+                                    'Ссылка на GitHub | GitHub link',
+                                    style=ft.TextStyle(
+                                        color=ft.colors.BLUE,
+                                        size=18
+                                    ),
+                                    url='https://github.com/Shido-Itsuka/vigenere_cipher'
+                                )
+                            ]
+                        ),
+                        ft.Divider(
+                            thickness=2,
+                            height=25,
+                            # color=ft.colors.TRANSPARENT,
+                        ),
+                        ft.Text(
+                            spans=[
+                                ft.TextSpan(
+                                    text='Copyright © 2024 | ',
+                                    style=ft.TextStyle(
+                                        size=16
+                                    )
+                                ),
+                                ft.TextSpan(
+                                    text='Shido-Itsuka',
+                                    style=ft.TextStyle(
+                                        color=ft.colors.BLUE,
+                                        size=16
+                                    ),
+                                    url='https://github.com/Shido-Itsuka'
+                                ),
+                                ft.TextSpan(
+                                    text=' | All rights reserved.',
+                                    style=ft.TextStyle(
+                                        size=16
+                                    )
+                                )
+                            ],
+                            style=ft.TextThemeStyle.LABEL_LARGE
+                        )
                     ]
                 )
             ]
